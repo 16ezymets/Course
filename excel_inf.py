3d9616d97e151b506ee67fc62b055bbd455a5901
16ezymets <alesha.fomin2007@gmail.com>
2023-05-25 14:23:47 +0300

    формулы

--- excel_inf.py
+++ excel_inf.py
@@ -1,8 +1,10 @@
 from app import App
 
 
+# Запись данных в текстовый файл
 def write_to_text_file(app: App):
-    with open(r'info_for_excel.txt', 'w') as file:
+    filename = 'info_for_excel.txt'
+    with open(filename, 'w') as file:
         for i in range(len(app.time)):
             line = f"{app.time[i]};{app.press[i]};{app.volume[i]};{app.temperature[i]}\n"
             file.write(line)
